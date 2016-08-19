213b69bf94f7f8bde7272ec8875dcadd28a63c48
Jason Yoon <jasony0206@gmail.com>
2016-08-18 21:46:37 -0700

    Add mock JSON response and start rendering the data

---
+++ json_response.js
@@ -0,0 +1,280 @@
+jsonResponse = 
+`
+{
+  "data": [
+    {
+      "name": "Chop'd",
+      "rating": 5,
+      "review_count": 80,
+      "location": {
+        "latitude": 33.9160746031551,
+        "longitude": -118.010964931696
+      },
+      "total_travel": {
+        "distance": {
+          "text": "38.8 mi",
+          "value": 62389
+        },
+        "duration": {
+          "text": "1 h 6 min",
+          "value": 3961
+        }
+      },
+      "to_restaurant": {
+        "distance": {
+          "text": "11.4 mi",
+          "value": 18343
+        },
+        "duration": {
+          "text": "21 mins",
+          "value": 1244
+        }
+      },
+      "from_restaurant": {
+        "distance": {
+          "text": "27.4 mi",
+          "value": 44046
+        },
+        "duration": {
+          "text": "45 mins",
+          "value": 2717
+        }
+      }
+    },
+    {
+      "name": "Tacos La Bomba",
+      "rating": 5,
+      "review_count": 38,
+      "location": {
+        "latitude": 33.9173349,
+        "longitude": -118.0713541
+      },
+      "total_travel": {
+        "distance": {
+          "text": "37.0 mi",
+          "value": 59548
+        },
+        "duration": {
+          "text": "56 min",
+          "value": 3394
+        }
+      },
+      "to_restaurant": {
+        "distance": {
+          "text": "12.9 mi",
+          "value": 20790
+        },
+        "duration": {
+          "text": "20 mins",
+          "value": 1202
+        }
+      },
+      "from_restaurant": {
+        "distance": {
+          "text": "24.1 mi",
+          "value": 38758
+        },
+        "duration": {
+          "text": "37 mins",
+          "value": 2192
+        }
+      }
+    },
+    {
+      "name": "Grilled Fraiche",
+      "rating": 5,
+      "review_count": 25,
+      "location": {
+        "latitude": 34.0328,
+        "longitude": -118.34531
+      },
+      "total_travel": {
+        "distance": {
+          "text": "48.3 mi",
+          "value": 77727
+        },
+        "duration": {
+          "text": "1 h 10 min",
+          "value": 4217
+        }
+      },
+      "to_restaurant": {
+        "distance": {
+          "text": "32.4 mi",
+          "value": 52138
+        },
+        "duration": {
+          "text": "44 mins",
+          "value": 2629
+        }
+      },
+      "from_restaurant": {
+        "distance": {
+          "text": "15.9 mi",
+          "value": 25589
+        },
+        "duration": {
+          "text": "26 mins",
+          "value": 1588
+        }
+      }
+    },
+    {
+      "name": "Napa Rose",
+      "rating": 4.5,
+      "review_count": 1210,
+      "location": {
+        "latitude": 33.80868282158,
+        "longitude": -117.92306921029
+      },
+      "total_travel": {
+        "distance": {
+          "text": "37.3 mi",
+          "value": 59989
+        },
+        "duration": {
+          "text": "57 min",
+          "value": 3450
+        }
+      },
+      "to_restaurant": {
+        "distance": {
+          "text": "1.3 mi",
+          "value": 2083
+        },
+        "duration": {
+          "text": "6 mins",
+          "value": 347
+        }
+      },
+      "from_restaurant": {
+        "distance": {
+          "text": "36.0 mi",
+          "value": 57906
+        },
+        "duration": {
+          "text": "52 mins",
+          "value": 3103
+        }
+      }
+    },
+    {
+      "name": "Joe's Falafel",
+      "rating": 4.5,
+      "review_count": 1173,
+      "location": {
+        "latitude": 34.1333008,
+        "longitude": -118.3573532
+      },
+      "total_travel": {
+        "distance": {
+          "text": "37.3 mi",
+          "value": 60064
+        },
+        "duration": {
+          "text": "55 min",
+          "value": 3359
+        }
+      },
+      "to_restaurant": {
+        "distance": {
+          "text": "36.3 mi",
+          "value": 58383
+        },
+        "duration": {
+          "text": "51 mins",
+          "value": 3046
+        }
+      },
+      "from_restaurant": {
+        "distance": {
+          "text": "1.0 mi",
+          "value": 1681
+        },
+        "duration": {
+          "text": "5 mins",
+          "value": 313
+        }
+      }
+    },
+    {
+      "name": "The Pizza Press",
+      "rating": 4.5,
+      "review_count": 950,
+      "location": {
+        "latitude": 33.810211,
+        "longitude": -117.91492
+      },
+      "total_travel": {
+        "distance": {
+          "text": "36.5 mi",
+          "value": 58781
+        },
+        "duration": {
+          "text": "54 min",
+          "value": 3244
+        }
+      },
+      "to_restaurant": {
+        "distance": {
+          "text": "0.5 mi",
+          "value": 759
+        },
+        "duration": {
+          "text": "4 mins",
+          "value": 230
+        }
+      },
+      "from_restaurant": {
+        "distance": {
+          "text": "36.1 mi",
+          "value": 58022
+        },
+        "duration": {
+          "text": "50 mins",
+          "value": 3014
+        }
+      }
+    },
+    {
+      "name": "Tacos Baja Ensenada",
+      "rating": 4.5,
+      "review_count": 880,
+      "location": {
+        "latitude": 34.020057,
+        "longitude": -118.154411
+      },
+      "total_travel": {
+        "distance": {
+          "text": "38.3 mi",
+          "value": 61596
+        },
+        "duration": {
+          "text": "1 h 2 min",
+          "value": 3741
+        }
+      },
+      "to_restaurant": {
+        "distance": {
+          "text": "21.8 mi",
+          "value": 35070
+        },
+        "duration": {
+          "text": "32 mins",
+          "value": 1929
+        }
+      },
+      "from_restaurant": {
+        "distance": {
+          "text": "16.5 mi",
+          "value": 26526
+        },
+        "duration": {
+          "text": "30 mins",
+          "value": 1812
+        }
+      }
+    }
+  ]
+}
+`;
